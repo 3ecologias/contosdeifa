f5f4f83b8f3913a0ec504dc90ba709a51086e1fb
CristianoLopes <lopes144@gmail.com>
2016-09-23 18:36:23 -0300

    Novas Atualiza

---
+++ games/Ibeji/js/vacuo.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('body').queryLoader2({
+        barColor: "#6fd556",
+        backgroundColor: "##790000",
+        percentage: true,
+        barHeight: 1,
+        completeAnimation: "grow",
+        minimumTime: 100
+    });
+});
